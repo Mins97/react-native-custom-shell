82d5fc2a97d51e8c57306e0e69afc53548d5b478
Mina97 <tkarnrwl78627862@gmail.com>
2021-01-08 11:47:01 +0900

    starter updated

--- new_project.sh
+++ new_project.sh
@@ -8,5 +8,4 @@ echo sdk.dir = /Users/$MacName/Library/Android/sdk > local.properties &&
 cd .. &&
 export ANDROID_SDK=/Users/$MacName/Library/Android/sdk &&
 echo 'export PATH=/Users/$MacName/Library/Android/sdk/platform-tools:/Users/mac/.nvm/versions/node/v12.13.0/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/mac/.nvm/versions/node/v12.13.0/bin:/Users/mac/.rvm/bin' >>~/.bash_profile &&
-source ~/.bash_profile &&
-rm -rf ~/Desktop/starter
+source ~/.bash_profile
