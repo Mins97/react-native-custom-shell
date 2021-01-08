b7a3de5c2d634226e4310ba3705c6310e756f9d0
Mina97 <tkarnrwl78627862@gmail.com>
2021-01-08 11:30:00 +0900

    starter updated

--- new_project.sh
+++ new_project.sh
@@ -1,24 +1,3 @@
-react-native-custom-shell
-# 사용자 맞춤으로 만든 쉘 스크립트 파일 모음입니다.
-# 리액트 네이티브 새 프로젝트 생성
-```
-sh starter.sh
-```
-```
-new_project.sh
-```
-starter는 테스트용 리액트네이티브 프로젝트(빠른 시작)이고,
-new_project는 새 프로젝트입니다.
-
-### sh starter.sh 내용
-```
-cd ~/Desktop &&
-git clone https://github.com/Mins97/react_native_mac_starter.git starter &&
-cd ~/Desktop/starter
-```
-
-### new_project.sh 내용
-```
 ROOT=~/Desktop
 AppName=myNewApp
 MacName=mac
@@ -31,4 +10,3 @@ export ANDROID_SDK=/Users/$MacName/Library/Android/sdk &&
 echo 'export PATH=/Users/$MacName/Library/Android/sdk/platform-tools:/Users/mac/.nvm/versions/node/v12.13.0/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/mac/.nvm/versions/node/v12.13.0/bin:/Users/mac/.rvm/bin' >>~/.bash_profile &&
 source ~/.bash_profile &&
 rm -rf ~/Desktop/starter
-```
--- starter.sh
+++ starter.sh
@@ -1,14 +1,3 @@
-ROOT=~/Desktop
-AppName=myNewApp
-MacName=mac
 cd ~/Desktop &&
 git clone https://github.com/Mins97/react_native_mac_starter.git starter &&
-cd ~/Desktop/starter &&
-cd $ROOT &&
-react-native init $AppName --version react-native@0.60.0 &&
-cd $ROOT/$AppName/android &&
-echo sdk.dir = /Users/$MacName/Library/Android/sdk > local.properties &&
-cd .. &&
-export ANDROID_SDK=/Users/$MacName/Library/Android/sdk &&
-echo 'export PATH=/Users/$MacName/Library/Android/sdk/platform-tools:/Users/mac/.nvm/versions/node/v12.13.0/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/mac/.nvm/versions/node/v12.13.0/bin:/Users/mac/.rvm/bin' >>~/.bash_profile &&
-source ~/.bash_profile
+cd ~/Desktop/starter
