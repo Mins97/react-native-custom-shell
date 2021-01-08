24b51192c29fdc129961d75903e0aefa9b487102
Mins97 <tkarnrwl7862@naver.com>
2021-01-08 12:11:57 +0900

    Update starter.sh

--- starter.sh
+++ starter.sh
@@ -2,4 +2,8 @@ cd ~/Desktop &&
 git clone https://github.com/Mins97/react_native_mac_starter.git starter &&
 cd ~/Desktop/starter &&
 cd testapp &&
-sh init.sh
+sh init.sh &&
+export ANDROID_SDK=/Users/$MacName/Library/Android/sdk &&
+echo 'export PATH=/Users/'$MacName'/Library/Android/sdk/platform-tools:/Users/'$MacName'/.nvm/versions/node/v12.13.0/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/'$MacName'/.nvm/versions/node/v12.13.0/bin:/Users/'$MacName'/.rvm/bin' >>~/.bash_profile &&
+source ~/.bash_profile &&
+echo source ~/.bash_profile > ~/.zshenv && source ~/.zshenv
